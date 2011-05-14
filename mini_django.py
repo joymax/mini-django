e34600e5c5702b76ffd83965a5db3560cdd79899
Max Klymyshyn <klymyshyn@gmail.com>
2011-05-14 16:17:13 +0300

    Added ability to start mini_django with devserver on board. Now possible to handle requests simultaneously which is more 'production-ready' solution. Also fixed PEP8 and added database by default

--- mini_django.py
+++ mini_django.py
@@ -2,7 +2,9 @@
 Run this with $ python ./micro_django.py and go to http://localhost:8000/Foo
 '''
 
-import os, sys
+import os
+import sys
+from django.core import management
 from django.conf.urls.defaults import patterns
 from django.template.response import TemplateResponse
 
@@ -12,22 +14,34 @@ me = os.path.splitext(os.path.split(__file__)[1])[0]
 here = lambda x: os.path.join(os.path.abspath(os.path.dirname(__file__)), x)
 
 # SETTINGS
-DEBUG=TEMPLATE_DEBUG=True
+DEBUG = True
+TEMPLATE_DEBUG = True
 ROOT_URLCONF = me
-DATABASES = { 'default': {} } #required regardless of actual usage
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': './test.db',
+    },
+}   # required regardless of actual usage
 TEMPLATE_DIRS = (here('.'), )
+INSTALLED_APPS = (
+'devserver',
+)
+
 
 # VIEW
 def index(request, name):
     return TemplateResponse(request, 'index.html', {'name': name})
 
+
 # URLS
 urlpatterns = patterns('', (r'^(?P<name>\w+)?$', index))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     # set the ENV
     os.environ['DJANGO_SETTINGS_MODULE'] = me
     sys.path += (here('.'),)
     # run the development server
-    from django.core import management
-    management.call_command('runserver','0.0.0.0:8000' )
+
+    management.call_command('runserver', '0.0.0.0:8000')
